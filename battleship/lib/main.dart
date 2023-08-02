da86b4982738c4b49de91ac34c769f9b317c9cc1
Anadi Sharma <sharma.130@iitj.ac.in>
2023-08-02 20:47:42 +0530

    Home page made

--- battleship/lib/main.dart
+++ battleship/lib/main.dart
@@ -19,11 +19,44 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(primarySwatch: Colors.teal),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text("Battleship"),
         ),
-        body: Container(
-          color: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    )
+                  ),
+                  onPressed: (){},
+                  child: Text("Create New Game"),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                      textStyle: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      )
+                  ),
+                  onPressed: (){},
+                  child: Text("     Join a Game     "),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
